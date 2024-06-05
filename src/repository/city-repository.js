ebb35f3cc584708e289bda2bf32bcf9d120c082f
kiran <kiran.s.dhawan@gmail.com>
2024-06-05 13:35:06 +0530

    Added service

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -25,7 +25,7 @@ class CityRepository {
     }
   }
 
-  async updateCity(cityId, data ) {
+  async updateCity(cityId, data ) {  //data ->{name:'Mumbai'}
     try {
       const city = await City.update(data,{
         where: {
